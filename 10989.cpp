20d49f3beec7109ccd372007488b42f40e2ae595
seoyoung-3060 <ksy654333@gmail.com>
2022-03-07 13:17:16 +0900

    time limit solved

--- 10989.cpp
+++ 10989.cpp
@@ -7,14 +7,17 @@ int main(){
     cout.tie(NULL);
     int a[10001] = {0,};
     int n;
-    cin >> n;
+    scanf("%d", &n);
     for(int i=0; i<n; i++){
         int in;
-        cin >> in;
+        scanf("%d",&in);
         a[in]++;
-    }
+    } //하나하나 다 저장하면 시간초과 -> count함수구현
     for(int i=1; i<=10000; i++){
-        for(int j=0; j<a[i]; j++) cout << i << endl;
-    }
+        for(int j=0; j<a[i]; j++) {
+            printf("%d",i);
+            printf("\n");
+        }
+    }//cin cout endl 시간 매우 느림 -> printf,scanf,\n활용!
     return 0;
 }
