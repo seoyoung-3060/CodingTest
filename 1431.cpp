f95116c7092c2faabb6bac35378fdb03a34a96d1
seoyoung-3060 <ksy654333@gmail.com>
2022-03-08 20:26:22 +0900

    baekjoon

---
+++ 1431.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+int n;
+
+bool comp(string a, string b){
+    if(a.size()!=b.size()){
+        return a.size() < b.size();
+    }
+    else{
+        int as = 0;
+        for(int i=0; i<a.size(); i++){
+            if('0'<=a[i] && a[i]<='9'){
+                as += (a[i]-'0');
+            }
+        }
+        int bs = 0;
+        for(int i=0; i<b.size(); i++){
+            if('0'<=b[i] && b[i]<='9'){
+                bs += (b[i]-'0');
+            }
+        }
+        if(as != bs) return as < bs;
+        else return a < b;
+    }
+}
+
+int main() {
+    vector<string> inp;
+
+    cin >> n;
+    while(n--){
+        string a;
+        cin >> a;
+        inp.push_back(a);
+    }
+    sort(inp.begin(), inp.end(), comp);
+    for(auto x : inp) cout << x << "\n";
+    return 0;
+}
